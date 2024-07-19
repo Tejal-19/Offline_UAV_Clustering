cab346d1282a236d647ca483f3c2086d8ebe47b2
Tejal <tbedmutha_b19@tx.vjti.ac.in>
2024-07-19 13:14:02 +0530

    Delete similar folders

--- momvo/momvo_multi_h/initmed.m
+++
@@ -1,8 +0,0 @@
-function [index] = initmed(U_id, Du, Dt ,k)
-
-ranks = non_dominated_ranking([Du, Dt], 2, length(U_id));
-[~,index] = sort(ranks, 'ascend');
-index=index(1:k,:);
-% med=U_id(index,:);
-
-end
--- momvo/momvo_multi_h/initmem.m
+++
@@ -1,8 +0,0 @@
-function [sdist, cid, dist] = initmem(D_u_u,index)
-
-dist = D_u_u(index,:);
-
-[nearest,cid] = min(dist);
-sdist = sum(normr(nearest));
-
-end
